a426a23560790ae7298f5a2048c94fb788c4004d
Thib454 <thibaultdepeige@gmail.com>
2026-01-06 10:33:54 +0100

    configuration database SQlite

---
+++ config/database.js
@@ -0,0 +1,5 @@
+const { Sequelize } = require('sequelize');
+
+const db = new Sequelize({ dialect: 'sqlite', storage: './database.sqlite', logging: false });
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 require('dotenv').config();
 
 const express = require('express');
-const app = express();
+const db = require('./config/database');
+const authRouter = require('./router/authRouter');
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerSpec = require('./config/swagger');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middlewares
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello Express avec dotenv');
-});
+// Routes
+app.use('/auth', authRouter);
 
-app.listen(PORT, () => {
-  console.log(`Serveur lancé sur http://localhost:${PORT}`);
-});
+// Swagger (à activer une fois swagger.js créé)
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+const initDatabase = async () => {
+  try {
+    await db.sync({ alter: true });
+    console.log('Base de données initialisée');
+
+    app.listen(PORT, () => {
+      console.log(`Serveur démarre sur http://localhost:${PORT}`);
+      console.log(`Documentation Swagger disponible sur http://localhost:${PORT}/api-docs`);
+    });
+  } catch (error) {
+    console.error('Erreur lors de l\'initialisation de la DB:', error);
+  }
+};
+
+initDatabase();
+
+module.exports = app;
