2a0561f7ad4d36afb5552c7da0340e383c7364c7
Thib454 <thibaultdepeige@gmail.com>
2026-01-06 09:50:21 +0100

    Initialisation serveur Express avec configuration dotenv

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello Express avec dotenv');
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
